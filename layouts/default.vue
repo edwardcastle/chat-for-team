0e27e3a01538f5d798c5e9d1f75b194ef25ef4f7
_edwardcastle <ecastle@elkanodata.com>
2025-02-03 01:57:01 +0100

    feat: add loading indicator in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="min-h-screen">
+  <div class="layout-container">
+    <NuxtLoadingIndicator :height="3" color="#3b82f6" />
     <slot />
   </div>
 </template>
+
+<style>
+.layout-container {
+  @apply min-h-screen bg-gray-50;
+}
+</style>
