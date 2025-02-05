21fbaf23eac7cb56efbba35cfc6c5c4cc409cec9
_edwardcastle <sir.edwardcastle@gmail.com>
2025-02-05 16:36:01 +0100

    perf: format code

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,14 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-    const supabase = useSupabaseClient()
-    const { data: { user } } = await supabase.auth.getUser()
+  const supabase = useSupabaseClient();
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
 
-    const publicRoutes = ['/login', '/register']
+  const publicRoutes = ['/login', '/register'];
 
-    if (!user && !publicRoutes.includes(to.path)) {
-        return navigateTo('/login', { replace: true })
-    }
+  if (!user && !publicRoutes.includes(to.path)) {
+    return navigateTo('/login', { replace: true });
+  }
 
-    if (user && publicRoutes.includes(to.path)) {
-        return navigateTo('/', { replace: true })
-    }
-})
+  if (user && publicRoutes.includes(to.path)) {
+    return navigateTo('/', { replace: true });
+  }
+});
